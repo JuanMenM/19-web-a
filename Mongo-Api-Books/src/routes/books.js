65959776ca2ed151c71a0602de76b856c069c17b
dashfuentes <smis117112@gmail.com>
2023-04-19 21:27:59 -0600

    feat:update books pending result

--- Mongo-Api-Books/src/routes/books.js
+++ Mongo-Api-Books/src/routes/books.js
@@ -3,6 +3,7 @@ const router = Router();
 
 //Import DB Connection
 import { connect } from '../database.js';
+import { ObjectId } from "mongodb";
 
 
 //Get books
@@ -17,10 +18,46 @@ router.get( '/books', async ( req, res ) => {
 })
 
 //Create books
+router.post( '/create-book', async( req, res ) => {
+    //Access to the db
+    const db = await connect();
+    //Get the request body to insert in the DB
+    console.log( req.body )
+    //#1
+    const book = {
+        title: req.body.title,
+        description: req.body.description
+    }
+    //#2
+    // const { title, description } = req.body
+    // const book = {title,description}
 
+    ///Create book in the DB
+    const result = await db.collection( 'books' ).insertOne( book );
+    console.log(result)
+
+    res.json( {
+        code: "201",
+        result: result.insertedId
+    })
+
+})
 
 //Update books
-
+router.put( '/update-book/:bookid', async( req, res ) => {
+    //get the bookid parameter 
+    const bookid = req.params.bookid;
+   // console.log( bookid )
+    const bookUpdate = {
+        title: req.body.title,
+        description: req.body.description
+    }
+    const db = await connect();
+    //Pending Update 
+    //const result = await db.collection( 'books' ).updateOne( { _id: bookid }, { $set: bookUpdate } )
+    //console.log(result)
+    return true
+})
 
 //Delete books
 
--- Mongo-Api-Books/src/server.js
+++ Mongo-Api-Books/src/server.js
@@ -1,5 +1,5 @@
 /** this file will include global settings about the server **/
-import express from "express";
+import express,{json} from "express";
 import indexRoutes from './routes/index.js'
 import bookRoutes from './routes/books.js'
 const app = express();
@@ -9,6 +9,7 @@ app.set( 'port', 3000 );
 
 
 //Middlewares
+app.use(json())
 
 //Routes
 app.use( indexRoutes )
