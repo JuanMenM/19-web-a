a4908c1e7929a8ce3a360efd2c86af88ac9266dc
dashfuentes <smis117112@gmail.com>
2023-04-26 21:24:26 -0600

    feat:create login code base

--- Mongo-MVC-Employees/src/app.js
+++ Mongo-MVC-Employees/src/app.js
@@ -3,11 +3,12 @@ import express from 'express';
 const app = express();
 
 //----- Import Routes -----//
-
+import authenticationRoutes from './routes/User.Routes.js'
 //---- Middlewares ------///
 
 //-- Global Settings ---- //
 app.use( express.json() );
+app.use(authenticationRoutes)
 
 export default app;
 
--- Mongo-MVC-Employees/src/controllers/User.Controller.js
+++ Mongo-MVC-Employees/src/controllers/User.Controller.js
@@ -1,16 +1,42 @@
 import User from '../models/User.js';
 import bcrypt from "bcryptjs";
 
-const signup = async( req, res ) => {
+export const signup = async( req, res ) => {
     const { name, email, password } = req.body;
     //Encrypt Password before insert
     //123 =  Hash = 6gdhdffifoflflfufunvnvnvnvlsrumdsgasd   Salt =  10 random words
     const encryptPassword = bcrypt.hashSync( password, 10 );
 
     const newUser = new User( { name: name, email: email, password: encryptPassword, role: "admin" } );
-    await newUser.save();
 
-    console.log( newUser );
-    return true   
+    try {
+        await newUser.save();
+       return res.json({code: 200, message:"The user was created in the DB!!"})
+    } catch (error) {
+       return res.json({message: "We could not create the user please check your information"})
+    }
+      
+}
 
+export const login = async ( req, res ) => {
+    const { email, password } = req.body;
+
+    //Find the user email in the database and verify if exists
+    const findEmail = await User.find( { email: email } ).exec();
+    console.log( findEmail );
+
+    //If does not exist send a validation message
+    if ( findEmail.length == 0 ) {
+        res.json({"code": 400, "message": "this email does not exist!!"})
+    } else {
+        ////
+        //If it does, Verify the password
+        //Create JWT and send it to the client
+        
+    }
+    return true
+
+  
+
+    
 }
